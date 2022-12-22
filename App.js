71b6031b99a938cc1710884b198cd9ec1be25060
ynitin2906 <ynitin2906@gmail.com>
2022-12-23 02:15:50 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,1 +1,19 @@
+const codes = document.querySelectorAll(".inp");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key < 9) {
+      codes[index].value = "";
+      setTimeout(() => {
+        codes[index + 1].focus();
+      }, 10);
+    } else if (e.key === "Backspace") {
+      setTimeout(() => {
+        codes[index - 1].focus();
+      }, 10);
+    }
+  });
+});
 
